3070bf9c63ce908a2f277a5e779be12ea202e435
Jason Li <jasonli.0616@outlook.com>
2021-04-11 16:50:34 -0400

    Refactor code + add comments

--- source code/PyInstaller GUI.py
+++ source code/PyInstaller GUI.py
@@ -4,35 +4,27 @@ import os
 import subprocess
 import webbrowser
 
+# initialize windoww
 root = Tk()
 s = ttk.Style()
 root.geometry('600x500')
 root.title('PyInstaller GUI')
 
+# configure page title style
 s.configure("Title.TLabel", font=('*', 25), padding=15)
 
+# creates a page title
 title = ttk.Label(root, text='PyInstaller GUI', style='Title.TLabel')
 title.pack()
 
+# creates div for pip/pip3 install pyinstaller
 pipFrame = ttk.Frame(root, padding=15)
 pipFrame.pack()
 
-pipVar = IntVar()
-pip3Var = IntVar()
-pipCheck = ttk.Checkbutton(pipFrame, text='pip', variable=pipVar)
-pipCheck.pack(side=LEFT)
-pipCheck3 = ttk.Checkbutton(pipFrame, text='pip3', variable=pip3Var)
-pipCheck3.pack(side=LEFT)
-
-def newWin(geoStr, showTitle, Lab1Str):
-    newWin = Toplevel()
-    newWin.geometry(geoStr)
-    winTitleLab = ttk.Label(newWin, text=showTitle, padding=5)
-    winTitleLab.pack()
-    winLab1 = ttk.Label(newWin, text=Lab1Str, padding=5)
-    winLab1.pack()
-
 def installPyInstaller():
+    '''
+    Function for button, installs pyinstaller in cmd prompt
+    '''
     if str(pipVar.get()) == '1' and str(pip3Var.get()) == '1':
         newWin('350x200', 'Error:', 'pip and pip3 are both selected')
     elif str(pipVar.get()) == '0' and str(pip3Var.get()) == '0':
@@ -44,7 +36,36 @@ def installPyInstaller():
         os.system('pip3 install pyinstaller')
         newWin('350x200', 'Successfully installed PyInstaller', '')
 
+
+# install pyinstaller button
+pipVar = IntVar()
+pip3Var = IntVar()
+pipCheck = ttk.Checkbutton(pipFrame, text='pip', variable=pipVar)
+pipCheck.pack(side=LEFT)
+pipCheck3 = ttk.Checkbutton(pipFrame, text='pip3', variable=pip3Var)
+pipCheck3.pack(side=LEFT)
+installBtn = ttk.Button(pipFrame, text='Install PyInstaller', command=installPyInstaller)
+installBtn.pack(side=LEFT)
+
+
+def newWin(geoStr, showTitle, Lab1Str):
+    '''
+    Creates popup window with error/success message
+    This takes arguments for window size + text in window
+    '''
+    newWin = Toplevel()
+    newWin.geometry(geoStr)
+    winTitleLab = ttk.Label(newWin, text=showTitle, padding=5)
+    winTitleLab.pack()
+    winLab1 = ttk.Label(newWin, text=Lab1Str, padding=5)
+    winLab1.pack()
+
+
 def dirQuestionFunc():
+    '''
+    Question mark for file path
+    Opens new window with explanation
+    '''
     dirPath = Toplevel()
     dirPath.geometry('350x200')
     dirPathTitle = ttk.Label(dirPath, text='Python File Path:', padding=5)
@@ -54,18 +75,40 @@ def dirQuestionFunc():
     dirPathLab = ttk.Label(dirPath, text='(Starts from where this app is located)', padding=5, foreground='red')
     dirPathLab.pack()
 
+
 def oneFileQuestionFunc():
+    '''
+    Question mark for one file
+    Opens new window with explanation
+    '''
     newWin('400x200', 'What is \"One file\"?', 'This will compile your program into one .exe file')
-    
+
+
 def noConsoleQuestionFunc():
+    '''
+    Question mark for no console
+    Opens new window with explanation
+    '''
     newWin('400x200', 'What is \"No console\"?', 'This will not launch the command line for your program\nLeave this unchecked if you\'re making a command line app')
 
+
 def iconQuestionFunc():
+    '''
+    Question mark for custom icon
+    Opens new window with explanation
+    '''
     newWin('400x200', 'Custom icon format:', 'iconfile.ico')
 
+
 def dataQuestionFunc():
+    '''
+    Question mark for add data files
+    Opens new window with explanation
+    '''
     def whatIsData():
+        # opens browser tab with explanation for add data files
         webbrowser.open_new_tab('https://pyinstaller.readthedocs.io/en/stable/spec-files.html#adding-data-files')
+
     data = Toplevel()
     data.geometry('400x200')
     dataTitle = ttk.Label(data, text='Add data format:', padding=5)
@@ -77,31 +120,24 @@ def dataQuestionFunc():
 
 
 def runPyInstaller():
-    noRun = False
+    '''
+    Runs PyInstaller in cmd prompt with parameters taken from GUI
+    '''
 
+    # gets user defined parameters into strings
     filePathStr = str(dirPath.get())
-
     oneFile = str(oneFileVar.get())
-
     noConsole = str(noConsoleVar.get())
-
     iconPathCheck = str(iconVar.get())
     iconPathStr = str(iconPath.get())
-
     addDataCheck = str(dataVar.get())
     addDataFilesStr = str(dataIn.get())
+    noRun = False
 
-    '''
-    print(f"File path: {filePathStr}")
-    print(f"One file check: {oneFile}")
-    print(f"No console check: {noConsole}")
-    print(f"Icon path check: {iconPathCheck}")
-    print(f"Icon path: {iconPathStr}")
-    print(f"Add data check: {addDataCheck}")
-    print(f"Data files: {addDataFilesStr}")
-    '''
-
+    # list to run with subprocess.call()
     runList = ['pyinstaller']
+
+    # checks user parameters (checkboxes), appends to list
     if filePathStr:
         runList.append(filePathStr)
     else:
@@ -126,14 +162,16 @@ def runPyInstaller():
             noRun = True
             newWin('350x200', 'Error:', 'No data files entered')
 
+    # run pyinstaller in terminal if no errors and opens window with terminal command
     if noRun == False:
         newWin('500x150', 'Running in terminal:', runList)
-
         subprocess.call(runList)
 
+# creates div for checkboxes/entries
 checksFrame = ttk.Frame(root, padding=20)
 checksFrame.pack()
 
+# user input py file
 dirPathFrame = ttk.Frame(checksFrame, padding=5)
 dirPathFrame.pack()
 dirPathLab = ttk.Label(dirPathFrame, text='.py File Path')
@@ -143,6 +181,7 @@ dirPath.pack(side=LEFT)
 dirQuestion = ttk.Button(dirPathFrame, text='?', command=dirQuestionFunc, width=1)
 dirQuestion.pack()
 
+# user check onefile
 oneFileFrame = ttk.Frame(checksFrame, padding=5)
 oneFileFrame.pack()
 oneFileVar = IntVar()
@@ -151,6 +190,7 @@ oneFileCheck.pack(side=LEFT)
 oneFileQuestion = ttk.Button(oneFileFrame, text='?', command=oneFileQuestionFunc, width=1)
 oneFileQuestion.pack(side=LEFT)
 
+# user check noconsole
 noConsoleFrame = ttk.Frame(checksFrame, padding=5)
 noConsoleFrame.pack()
 noConsoleVar = IntVar()
@@ -159,6 +199,7 @@ noConsoleCheck.pack(side=LEFT)
 noConsoleQuestion = ttk.Button(noConsoleFrame, text='?', command=noConsoleQuestionFunc, width=1)
 noConsoleQuestion.pack(side=LEFT)
 
+# user check custom icon + user input icon file
 iconFrame = ttk.Frame(checksFrame, padding=5)
 iconFrame.pack()
 iconVar = IntVar()
@@ -171,6 +212,7 @@ iconPath.pack(side=LEFT)
 iconQuestion = ttk.Button(iconFrame, text='?', command=iconQuestionFunc, width=1)
 iconQuestion.pack(side=LEFT)
 
+# user check add data + user input data files
 dataFrame = ttk.Frame(checksFrame, padding=5)
 dataFrame.pack()
 dataVar = IntVar()
@@ -183,12 +225,9 @@ dataIn.pack(side=LEFT)
 dataQuestion = ttk.Button(dataFrame, text='?', command=dataQuestionFunc, width=1)
 dataQuestion.pack(side=LEFT)
 
+# run pyinstaller button
 runPyInstaller = ttk.Button(checksFrame, text='Run PyInstaller', command=runPyInstaller)
 runPyInstaller.pack()
-
-
-installBtn = ttk.Button(pipFrame, text='Install PyInstaller', command=installPyInstaller)
-installBtn.pack(side=LEFT)
 
 creditFrame = ttk.Frame(root, padding=10)
 creditFrame.pack()
